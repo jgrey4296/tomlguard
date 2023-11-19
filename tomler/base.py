9f29f50c84582a2fd353bb5459bfa0c04a7087de
John Grey <johngrey4296@gmail.com>
2023-11-19 22:42:39 +0000

    [chore]: remove unneeded more_itertools

--- tomler/base.py
+++ tomler/base.py
@@ -29,10 +29,6 @@ from uuid import UUID, uuid1
 
 ##-- end builtin imports
 
-##-- lib imports
-import more_itertools as mitz
-##-- end lib imports
-
 ##-- logging
 logging = logmod.getLogger(__name__)
 ##-- end logging
